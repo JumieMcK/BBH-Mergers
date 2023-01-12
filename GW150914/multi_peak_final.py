5d6b39e6b4063b7d9c016a9b83079a518ea5778f
graemejcrawford <114863962+graemejcrawford@users.noreply.github.com>
2023-01-12 08:44:22 +0000

    Update multi_peak_final.py

--- GW150914/multi_peak_final.py
+++ GW150914/multi_peak_final.py
@@ -236,97 +236,10 @@ class Interped(bilby.core.prior.Prior):
 prior_int = Interped(xx = x,  yy = y_i, minimum = 0.00001, maximum = 120)
 
 
-# Injecting parameters
-
-duration = 4
-sampling_frequency =  2048
-outdir = "multi_peak_prior"
-label = "example"
-
-injection_parameters = dict(
-	mass_1 = 36.0,
-	mass_2 = 29.0,
-	a_1 = 0.4,
-	a_2 = 0.3,
-	tilt_1 = 0.5,
-	tilt_2 = 1.0,
-	phi_12 = 1.7,
-	phi_jl = 0.3,
-	luminosity_distance = 1000.0,
-	theta_jn = 0.4,
-	phase = 1.3,
-	ra = 1.375,
-	dec = -1.2108,
-	geocent_time = 1126259542.413,
-	psi = 2.659,
-)
-
-
-# Defining the waveform arguments
-
-waveform_arguments = dict(
-	waveform_approximant = "IMRPhenomXP",
-	reference_frequency = 50.0,
-)
-
-# Defining the waveform generator
-
-waveform_generator = bilby.gw.waveform_generator.WaveformGenerator(
-	sampling_frequency = sampling_frequency,
-	duration = duration,
-	frequency_domain_source_model = bilby.gw.source.lal_binary_black_hole,
-	parameters = injection_parameters,
-	waveform_arguments = waveform_arguments,
-)
-
-
-# Setting up interferometers
-
-ifos = bilby.gw.detector.InterferometerList(["H1", "L1"])
-ifos.set_strain_data_from_power_spectral_densities(
-	duration=duration,
-	sampling_frequency = sampling_frequency,
-	start_time = injection_parameters["geocent_time"] - 2,
-)
-
-_ = ifos.inject_signal(
-	waveform_generator = waveform_generator, parameters = injection_parameters
-)
-
-# Define priors as a delta function at a particular value,
-# then manually change the mass and luminosity distance prior
-
-
-priors = bilby.gw.prior.BBHPriorDict(injection_parameters.copy())
-
-
-priors["mass_1"] = prior_int
-priors["mass_2"] = prior_int
-priors["luminosity_distance"] = bilby.core.prior.Uniform(400, 2000, "luminosity_distance")
-
-
-
-# Calculating Likelihood
-
-likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
-	interferometers = ifos, waveform_generator = waveform_generator)
-	
-    
-
-# Obtaining results
-
-result = bilby.core.sampler.run_sampler(
-	likelihood = likelihood,
-	priors = priors,
-	sampler = "dynesty",
-	npoints = 100,
-	injection_parameters = injection_parameters,
-	outdir = outdir,
-	label = label,
-	walks = 5,
-	nact = 2,
-)
-result.plot_corner()
+# In the examples the dictionary of priors is given the variable name 'priors' and the mass priors are named "mass_1" and "mass_2"
+# In order to replace the standard mass priors with this new one use: priors["mass_1"] = prior_int 
+# Similarly use: priors["mass_2"] = prior_int 
+ 
 
 
 
