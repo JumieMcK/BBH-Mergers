858db9d1be67c118f4287840ead85000ac7dcbde
JumieMcK <115263055+JumieMcK@users.noreply.github.com>
2023-01-17 11:06:55 +0000

    Update SCP.sh

--- Downloading Results/SCP.sh
+++ Downloading Results/SCP.sh
@@ -1,4 +1,4 @@
 #! usr/bin/bash
 
+#copies the XPwaveform directory to my laptops desktop from a linux terminal not connected to wiay.
 scp -r 2324304m@wiay.astro.gla.ac.uk:/data/wiay/undergrads/project781/GW150914/XPwaveform /mnt/c/Users/jamie/Desktop
-
